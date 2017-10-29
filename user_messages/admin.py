e659a3f02fe8755aaeb8fc03747714ce5d2296f5
sloniewski <sloniewski.maciej@gmail.com>
2017-10-29 16:25:21 +0100

    everything registered in admin

--- user_messages/admin.py
+++ user_messages/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.apps import apps
+import sys
+
+for myModel in apps.get_models():
+    try:
+        AdminModel = getattr(sys.modules[__name__], (myModel.__name__ + 'Admin'))
+        admin.site.register(myModel, AdminModel)
+    except Exception:
+        try:
+            admin.site.register(myModel)
+        except admin.sites.AlreadyRegistered:
+            pass
